ac0faf0fc6da772cc6e9ff71198bd3061c42b386
douglas godois dos santos <doug_potter92@hotmail.com>
2023-02-04 15:29:18 -0300

    atulaizando desafio

--- src/desafios/desafio3.ts
+++ src/desafios/desafio3.ts
@@ -1,22 +1,27 @@
 // O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
 
-let botaoAtualizar = document.getElementById('atualizar-saldo');
-let botaoLimpar = document.getElementById('limpar-saldo');
-let soma = document.getElementById('soma');
-let campoSaldo = document.getElementById('campo-saldo');
+let botaoAtualizar = document.getElementById('atualizar-saldo')as HTMLButtonElement;
+let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
+let soma = document.getElementById('soma') as HTMLInputElement ;
+let campoSaldo = document.getElementById('campo-saldo') as  HTMLInputElement;
 
-campoSaldo.innerHTML = 0
+let saldoTotal = 0
 
-function somarAoSaldo(soma) {
+limparSaldo()
+
+function somarAoSaldo(soma:number) {
     campoSaldo.innerHTML += soma;
 }
 
 function limparSaldo() {
-    campoSaldo.innerHTML = '';
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = campoSaldo.toString();
+    }
 }
 
 botaoAtualizar.addEventListener('click', function () {
-    somarAoSaldo(soma.value);
+    somarAoSaldo(Number(soma.value));
 });
 
 botaoLimpar.addEventListener('click', function () {
